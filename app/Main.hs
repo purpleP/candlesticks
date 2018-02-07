f77d2ecb44fbe7a7c4cc7ebd78312603b56facd9
Michael Doronin <warrior2031@gmail.com>
2018-02-07 17:01:52 +0300

    working

--- app/Main.hs
+++ app/Main.hs
@@ -2,8 +2,10 @@ module Main where
 
 import Data.Aeson
 import Data.Conduit
+import Data.Conduit.Lift
 import Data.Ord
 import Control.Monad.State
+import Data.ByteString.Lazy.Char8 as BL hiding (last, head, maximum, minimum, map)
 import qualified Data.Vector as V
 import qualified Data.Map.Lazy as Map
 import GHC.Exts (groupWith)
@@ -44,21 +46,54 @@ candles :: (Num a, Ord a) => [(Int, Int, a, a, a, a)] -> Map.Map Int (Map.Map In
 candles vs = Map.fromList $ Prelude.map (\(k, g) ->  (k, Map.fromList $ Prelude.map (\(t, gt) -> (t, candle $ V.fromList gt)) $ group' (\(_, x, _, _, _, _) -> x) g)) $ group' (\(x, _, _, _, _, _) -> x) vs
 
 update :: (Ord a) => Map.Map Int (Map.Map Int (a, a, a, a)) -> Map.Map Int (Map.Map Int (a, a, a, a)) -> Map.Map Int (Map.Map Int (a, a, a, a))
-update new old = Map.unionWith (Map.unionWith (\(mm1, mn1, o1, c1) (mm2, mn2, o2, c2) -> (max mm1 mm2, min mn1 mn2, o1, max c1 c2))) old new
+update new old = Map.unionWith (Map.unionWith (\(mm1, mn1, o1, c1) (mm2, mn2, o2, c2) -> (max mm1 mm2, min mn1 mn2, o2, max c1 c2))) old new
 
-diff' old new = Map.differenceWith (\ n o -> Just (Map.differenceWith (\ x y -> Just x) n o)) new old
+diff = Map.differenceWith d1
+    where
+        d1 x y = if Map.null diff' then Nothing else Just diff'
+            where diff' = Map.differenceWith d2 x y
+                    where
+                        d2 x y = if x == y then Nothing else Just x
 
-diff cs = do
+
+-- diff :: (Ord a) => Map.Map Int (Map.Map Int (a, a, a, a)) -> State (Map.Map Int (Map.Map Int (a, a, a, a))) (Map.Map Int (Map.Map Int (a, a, a, a)))
+-- diff cs = do
+--     old <- get
+--     let new = update cs old
+--     put new
+--     return $ diff old new
+
+
+-- diffConduit :: (Monad m, Ord a) => Conduit (Map.Map Int (Map.Map Int (a, a, a, a))) m (Map.Map Int (Map.Map Int (a, a, a, a)))
+-- diffConduit = evalStateC Map.empty $ awaitForever $ \cs -> do
+--     old <- get
+--     let new = update cs old
+--     put new
+--     yield $ diff old new
+
+candlesParser :: Parser (Int, Int, Int, Int, Int, Int)
+candlesParser = arrayOf $ (,,,,,) <$> arrayWithIndexOf 0 value
+                                  <*> arrayWithIndexOf 1 value
+                                  <*> arrayWithIndexOf 2 value
+                                  <*> arrayWithIndexOf 2 value
+                                  <*> arrayWithIndexOf 2 value
+                                  <*> arrayWithIndexOf 2 value
+
+printCandles = forever $ do
+    l <- liftIO $ BC.getLine
+    let cs = candles $ parseByteString candlesParser l
     old <- get
-    modify $ update cs
-    new <- get
-    return $ diff' old new
+    let new = update cs old
+    put new
+    let d = diff new old
+    liftIO $ if Map.null d then return () else BL.putStrLn $ encode d
 
-s = return Map.empty :: State (Map.Map Int (Map.Map Int (a, a, a, a))) (Map.Map Int (Map.Map Int (a, a, a, a)))
+main = runStateT printCandles Map.empty
 
-main = runConduit $ CB.sourceHandle stdin
-                 .| jsonParseValue ((arrayOf $ (,,,,,) <$> arrayWithIndexOf 0 value <*> arrayWithIndexOf 1 value <*> arrayWithIndexOf 2 value <*> arrayWithIndexOf 2 value <*> arrayWithIndexOf 2 value <*> arrayWithIndexOf 2 value) :: Parser (Int, Int, Int, Int, Int, Int))
-                 .| CC.map candles
-                 .| lift diff
-                 .| pack . show
-                 .| CB.sinkHandle stdout
+
+-- main = runConduit $ CB.sourceHandle stdin
+--                  .| jsonParseValue ((arrayOf $ (,,,,,) <$> arrayWithIndexOf 0 value <*> arrayWithIndexOf 1 value <*> arrayWithIndexOf 2 value <*> arrayWithIndexOf 2 value <*> arrayWithIndexOf 2 value <*> arrayWithIndexOf 2 value) :: Parser (Int, Int, Int, Int, Int, Int))
+--                  .| CC.map candles
+--                  .| diffConduit
+--                  .| CC.map (pack . show)
+--                  .| CB.sinkHandle stdout
